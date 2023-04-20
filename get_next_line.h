f939a00c9ef77b4a8acb2ae8ce072c3e87fac01d
Maxime Langlois <mlangloi@c2r2p2.42lehavre.fr>
2023-04-20 23:38:08 +0200

    add get_next_line

---
+++ get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlangloi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/20 22:25:23 by mlangloi          #+#    #+#             */
+/*   Updated: 2023/04/20 22:28:58 by mlangloi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 10
+#endif
+
+#include<stdlib.h>
+#include<unistd.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char *s);
+int		ft_clean(char *str);
+
+#endif
